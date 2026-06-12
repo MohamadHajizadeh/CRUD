c843dcbe6e4c1358c7cf2ef74c7b31d05d4b7c87
Mohamad <mohmadhajizadeh94@gmail.com>
2026-06-12 14:22:05 +0330

    improve products

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const Joi = require('joi');
 const mongoose = require('mongoose');
-const productModel = require('../models/productsModel');
+const Product = require('../models/productsModel');
 const { productsValidator } = require('../validators/productsValidators');
 // ##########################################################################
-// connect and create database
-
 
 const router = express.Router();
 
 //get a list of product
 router.get('/api/products', async (req, res) => {
     try {
-        const productList = await productModel.find();
-        await res.send(productList);
+        const productList = await Product.find();
+        await res.json(productList);
     } catch (error) {
         res.status(404).send(error.message)
     }
@@ -21,8 +19,9 @@ router.get('/api/products', async (req, res) => {
 // get a product
 router.get('/api/products/:id', async (req, res) => {
     try {
-        const product = await productModel.findById(req.params.id );
-        if(product) return res.send(product)
+        const product = await Product.findById(req.params.id);
+        if (!product) return res.status(404).json({ message: "not found" })
+        res.json(product)
     } catch (error) {
         console.log(error.message);
     }
@@ -33,7 +32,7 @@ router.post('/api/products', async (req, res) => {
 
     const { error } = productsValidator(req.body)
     if (error) return res.status(400).send({ error: error.message })
-    const newProduct = new productModel({
+    const newProduct = new Product({
         product_name: req.body.product_name,
         quantity: req.body.quantity,
         details: req.body.details,
@@ -41,23 +40,27 @@ router.post('/api/products', async (req, res) => {
         is_stock: req.body.is_stock
     })
     await newProduct.save();
-    res.send(newProduct)
+    res.status(201).json(newProduct)
     await console.log(`${newProduct.product_name} is inserted into database`);
 })
 // ###########################
 // update a product
 router.put('/api/products/:id', async (req, res) => {
     try {
-        const updatedProduct = await productModel.findByIdAndUpdate(req.params.id, {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
             product_name: req.body.product_name,
             quantity: req.body.quantity,
             details: req.body.details,
             price: req.body.price,
             is_stock: req.body.is_stock
+        }, {
+            new: true,
+            runValidators: true
         })
-        const{error} = productsValidator(updatedProduct);
-        if(error) return res.status(400).send({message: error.message})
-        await res.send(updatedProduct)
+        if (!updatedProduct) return res.status(400).send({ message: "product not found" })
+        const { error } = productsValidator(updatedProduct);
+        if (error) return res.status(400).send({ message: error.message })
+        await res.json(updatedProduct)
     } catch (error) {
         res.status(404).send({ message: error.message })
     }
@@ -66,8 +69,9 @@ router.put('/api/products/:id', async (req, res) => {
 // delete a product
 router.delete('/api/products/:id', async (req, res) => {
     try {
-        const deletedProduct = await productModel.findByIdAndDelete(req.params.id)
-        res.send(`${deletedProduct.product_name} is deleted`)
+        const deletedProduct = await Product.findByIdAndDelete(req.params.id)
+        if (!deletedProduct) return res.status(400).json({ messaage: "bad request" })
+        res.json(`${deletedProduct.product_name} is deleted`)
     } catch (error) {
         res.status(404).send(error.message)
     }
